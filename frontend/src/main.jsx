612b705a4b5f11673dd718d10822c76ba9a7aaa5
fiskryeziu <fiskryeziu@gmail.com>
2023-03-25 22:37:50 +0100

    changed to vite from cra

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,9 @@ import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
-import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
 )
-
-reportWebVitals()
